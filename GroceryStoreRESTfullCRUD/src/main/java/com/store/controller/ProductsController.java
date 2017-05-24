680a34600d6a93f93b9735f716134b40e541a265
OLENKA Andrushchakevych <dmalohlovets@chegg.com>
2017-05-24 22:06:11 +0300

    #9 Create ProductsDao and ProductsController

---
+++ GroceryStoreRESTfullCRUD/src/main/java/com/store/controller/ProductsController.java
@@ -0,0 +1,47 @@
+package com.store.controller;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.store.components.entity.Products;
+import com.store.service.ProductsService;
+
+@RestController
+@RequestMapping("/store/products")
+public class ProductsController {
+	@Autowired
+	private ProductsService productsService;
+
+	/**
+	 * Get products by id from DB
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value = "/view/{id}", method = RequestMethod.GET)
+	public Products searchByIdProducts(@PathVariable("id") int id) {
+		return productsService.searchByIdProducts(id);
+	}
+
+	/**
+	 * Get all products from DB
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value = "/information/{page}", method = RequestMethod.GET)
+	public Collection<Products> infoAboutProducts(@PathVariable("page") int page) {
+		return productsService.infoAboutProducts(page);
+	}
+	
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
+	public String addProduct(@RequestBody Products product) {
+		productsService.addProduct(product);
+		return "SUCCESS: Product was created!";
+	}
+}
