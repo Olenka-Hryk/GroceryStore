60231a609d3d3898d29d5f0f03955341506199c2
OLENKA Andrushchakevych <dmalohlovets@chegg.com>
2017-05-24 13:10:47 +0300

    #1 Create start clear project

---
+++ GroceryStoreRESTfullCRUD/src/main/java/com/store/GroceryStore/GroceryStoreResTfullCrudApplication.java
@@ -0,0 +1,12 @@
+package com.store.GroceryStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryStoreResTfullCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroceryStoreResTfullCrudApplication.class, args);
+	}
+}
---
+++ GroceryStoreRESTfullCRUD/src/test/java/com/store/GroceryStore/GroceryStoreResTfullCrudApplicationTests.java
@@ -0,0 +1,16 @@
+package com.store.GroceryStore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GroceryStoreResTfullCrudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
