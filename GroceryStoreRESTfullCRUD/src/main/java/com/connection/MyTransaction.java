b198d0ef5362c57f9a501fb666344a1fd1ed7718
OlenkaA <olenka49887@gmail.com>
2017-07-20 16:43:16 +0300

    Update MyTransaction.java

--- GroceryStoreRESTfullCRUD/src/main/java/com/connection/MyTransaction.java
+++ GroceryStoreRESTfullCRUD/src/main/java/com/connection/MyTransaction.java
@@ -9,21 +9,23 @@ import javax.transaction.NotSupportedException;
 import javax.transaction.RollbackException;
 import javax.transaction.SystemException;
 
+/**
+ * To abstract the database transaction we will use a class named MyTransaction.
+ * Class is just an abstraction to the database connection; it will help us with the database queries. 
+ * You could use any type of connection or you could use even a EJB to be avoiding this manually connection.
+ */
 public class MyTransaction implements Serializable {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-	
-	private Connection connection = new Connection();	
-	
+
+	private Connection connection = new Connection();
+
 	public void begin() throws NotSupportedException, SystemException {
 		connection.begin();
 	}
 
-	public void commit() throws RollbackException, HeuristicMixedException, HeuristicRollbackException, SecurityException, IllegalStateException,
-			SystemException {
+	public void commit() throws RollbackException, HeuristicMixedException, HeuristicRollbackException,
+			SecurityException, IllegalStateException, SystemException {
 		connection.commit();
 	}
 
