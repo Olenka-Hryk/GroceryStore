03fd6c4e1e451f617a6ae132682fe226118f4c5b
OlenkaA <olenka49887@gmail.com>
2017-08-01 14:36:47 +0300

    Update ReserveDao.java

--- GroceryStoreRESTfullCRUD/src/main/java/com/store/components/service/ReserveDao.java
+++ GroceryStoreRESTfullCRUD/src/main/java/com/store/components/service/ReserveDao.java
@@ -2,7 +2,6 @@ package com.store.components.service;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Collection;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -62,7 +61,7 @@ public class ReserveDao implements IReserve {
 	/**
 	 * Select all information about products
 	 */
-	public Collection<Reserve> infoAboutProduct(int page) {
+	public List<Reserve> infoAboutProduct(int page) {
 		page *= 3;
 		final String SQL = "SELECT * FROM Reserve LIMIT 3 offset ?";
 		List<Reserve> products = jdbc.query(SQL, new workingWithRowMap(), page);
@@ -71,15 +70,8 @@ public class ReserveDao implements IReserve {
 
 	public void addProduct(Reserve product) {
 		final String SQL = "INSERT INTO Reserve (idStore, idProduct, ExpirationDate, Quantity, Price, BarCode, ProductSize) values (?, ?, ?, ?, ?, ?, ?) ";
-		final int idStore = product.getIdStore();
-		final int idProduct = product.getIdProduct();
-		final String expirationDate = product.getExpirationDate();
-		final float quantity = product.getQuantity();
-		final float price = product.getPrice();
-		final String barCode = product.getBarCode();
-		final float productSize = product.getProductSize();
 
-		jdbc.update(SQL, new Object[] { idStore, idProduct, expirationDate, quantity, price, barCode, productSize });
+		jdbc.update(SQL,product.getIdStore(), product.getIdProduct(), product.getExpirationDate(), product.getQuantity(), product.getPrice(), product.getBarCode(), product.getProductSize());
 	}
 
 	/**
@@ -100,16 +92,7 @@ public class ReserveDao implements IReserve {
 
 	public void updateProduct(Reserve product) {
 		final String SQL = "UPDATE Reserve SET idStore=?, idProduct=?, ExpirationDate=?, Quantity=?, Price=?, BarCode=?, ProductSize=?   WHERE idProduct=?";
-		final int id = product.getId();
-		final int idStore = product.getIdStore();
-		final int idProduct = product.getIdProduct();
-		final String expirationDate = product.getExpirationDate();
-		final float quantity = product.getQuantity();
-		final float price = product.getPrice();
-		final String barCode = product.getBarCode();
-		final float productSize = product.getProductSize();
 
-		jdbc.update(SQL, new Object[] { id, idStore, idProduct, expirationDate, quantity, price, barCode, productSize });
+		jdbc.update(SQL,product.getIdStore(), product.getIdProduct(), product.getExpirationDate(), product.getQuantity(), product.getPrice(), product.getBarCode(), product.getProductSize());
 	}
-
 }
