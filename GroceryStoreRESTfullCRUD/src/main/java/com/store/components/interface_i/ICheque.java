e5250bf9571162fbccb6fe8db159c2709d03c15c
OlenkaA <olenka49887@gmail.com>
2017-08-01 15:05:29 +0300

    Update ICheque.java

--- GroceryStoreRESTfullCRUD/src/main/java/com/store/components/interface_i/ICheque.java
+++ GroceryStoreRESTfullCRUD/src/main/java/com/store/components/interface_i/ICheque.java
@@ -1,10 +1,11 @@
 package com.store.components.interface_i;
 
-import java.util.Collection;
+import java.util.List;
+
 import com.store.components.entity.Cheque;
 
 public interface ICheque {
-	Collection<Cheque> infoAboutAllCheque(int page);
+	List<Cheque> infoAboutAllCheque();
 
 	public Cheque infoAboutAmountOfOrderById(int id);
 	public void addCheque(Cheque cheque);
