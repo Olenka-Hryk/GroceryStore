f664d0dbf33098dc490aa1f3bfae25effa7f3d06
OlenkaA <olenka49887@gmail.com>
2017-08-01 15:25:36 +0300

    Update IOrders.java

--- GroceryStoreRESTfullCRUD/src/main/java/com/store/components/interface_i/IOrders.java
+++ GroceryStoreRESTfullCRUD/src/main/java/com/store/components/interface_i/IOrders.java
@@ -1,10 +1,10 @@
 package com.store.components.interface_i;
 
-import java.util.Collection;
+import java.util.List;
 import com.store.components.entity.Orders;
 
 public interface IOrders {
-	Collection<Orders> infoAboutAllOrder(int page);
+	List<Orders> infoAboutAllOrder();
 
 	public Orders infoAboutSumOfOrderById(int id);
 	public void addOrder(Orders order);
