e6a571c4c50d91e8c867311967a734b412965794
OlenkaA <olenka49887@gmail.com>
2017-08-01 15:57:45 +0300

    Update ITimeTable.java

--- GroceryStoreRESTfullCRUD/src/main/java/com/store/components/interface_i/ITimeTable.java
+++ GroceryStoreRESTfullCRUD/src/main/java/com/store/components/interface_i/ITimeTable.java
@@ -1,10 +1,10 @@
 package com.store.components.interface_i;
 
-import java.util.Collection;
+import java.util.List;
 import com.store.components.entity.TimeTable;
 
 public interface ITimeTable {
 	public TimeTable searchByIdWorker(int idWorker);
 
-	Collection<TimeTable> infoAboutTimeTable(int page);
+	List<TimeTable> infoAboutAllTimeTable();
 }
