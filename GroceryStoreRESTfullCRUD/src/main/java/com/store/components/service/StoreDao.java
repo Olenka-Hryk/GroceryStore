cc86958ed702d6d143b4bd49d484d48cd72cf8ac
OlenkaA <olenka49887@gmail.com>
2017-08-01 15:48:52 +0300

    Update StoreDao.java

--- GroceryStoreRESTfullCRUD/src/main/java/com/store/components/service/StoreDao.java
+++ GroceryStoreRESTfullCRUD/src/main/java/com/store/components/service/StoreDao.java
@@ -5,6 +5,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
@@ -50,10 +51,8 @@ public class StoreDao implements IStore{
 	 * @param page
 	 * @return store
 	 */
-	public List<Store> infoAboutStore(int page) {
-		page *= 3;
-		final String SQL = "SELECT * FROM Store LIMIT 3 offset ?";
-		List<Store> store = jdbc.query(SQL, new workingWithRowMap(), page);
+	public List<Store> infoAboutAllStore() {
+		List<Store> store = jdbc.query("SELECT * FROM Store", new BeanPropertyRowMapper(Store.class));
 		return store;
 	}
 
